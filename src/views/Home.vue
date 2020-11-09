4b98dab459ac05a4099de16b6df7ad2f20e46276
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-11-09 16:27:49 -0300

    names update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,85 @@
 <template>
-  <div class="home">
-    <h1></h1>
-  </div>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Nome
+          </th>
+          <th class="text-left">
+            Telefone
+          </th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+           v-for="name in result"
+          :key="result.name"
+          
+        >
+          <td>{{ name }}</td>
+          
+        </tr>
+        
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
+
+<script>
+import firebase from 'firebase' 
+import {db} from '../service/firebase'
+
+
+  export default {
+
+    
+    data () {   
+      
+      return {
+        userName: null,        
+        name: null,
+        email: null,       
+        userMail: null,
+        telefone: null,
+        userPhone: null,        
+        userAccept: null,
+
+        result: {
+          name: this.result, 
+          telefone: this.result2
+        }
+       
+      }
+    },
+    
+     async created() {
+       
+
+      var userData = []
+      await db.collection("users").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          console.log(`${doc.id} => ${doc.data()}`);
+          userData.push(doc.data())
+          
+          
+          
+          
+    });
+
+    
+});   
+      this.result = userData.map(a => a.name)
+      this.result2 = userData.map(a => a.telefone)
+      
+       console.log(result)
+      
+      
+    }
+    
+    
+  }      
+    
+</script>
+
